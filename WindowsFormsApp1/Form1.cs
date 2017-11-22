5d9f6590f5f9eeda7f10791a793a3c60d8efb371
Yuttitum Jituatrakoon <yuttitum.yut@gmail.com>
2017-11-22 14:18:42 +0700

    adding button1_Click function

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -19,7 +19,7 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Console.Write("Test");
         }
 
         public void test()
