b451ccb64c0ea5b5a5867b195668aba9fde3a039
Yuttitum Jituatrakoon <yuttitum.yut@gmail.com>
2017-11-22 14:04:48 +0700

    Test 2

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,12 +16,6 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
         public void test()
         {
 
